4ea4ff4dd1a3e29fad8b532ddb7fb8148d4d37c0
ghroth <229089241@qq.com>
2025-08-24 16:36:26 +0800

    更新.gitignore以限制以tfw_开头的可执行文件链接仅限根目录；在CMakeLists.txt中替换线程安全源文件名，并新增线程安全互斥锁操作函数实现，增强代码的安全性和可维护性。

---
+++ utils/thread/TFW_thread.c
@@ -0,0 +1,47 @@
+#include "TFW_thread.h"
+
+/**
+ * 安全的互斥锁加锁操作
+ * @param mutex 互斥锁指针
+ * @return 0表示成功，负数表示错误
+ */
+int32_t TFW_Mutex_Lock_Safe(TFW_Mutex_t* mutex)
+{
+    int32_t ret = TFW_SUCCESS;
+
+    if (TFW_CheckMutexIsNull(mutex)) {
+        TFW_LOGD_UTILS("TFW_Mutex_Lock mutex is null");
+        ret = TFW_ERROR_INVALID_PARAM;
+    } else {
+        ret = TFW_Mutex_Lock_Inner(mutex);
+        if (ret != 0) {
+            TFW_LOGE_UTILS("TFW_Mutex_Lock failed, ret=%d", ret);
+            ret = TFW_ERROR_LOCK_FAILED;
+        }
+    }
+
+    return ret;
+}
+
+/**
+ * 安全的互斥锁解锁操作
+ * @param mutex 互斥锁指针
+ * @return 0表示成功，负数表示错误
+ */
+int32_t TFW_Mutex_Unlock_Safe(TFW_Mutex_t* mutex)
+{
+    int32_t ret = TFW_SUCCESS;
+
+    if (TFW_CheckMutexIsNull(mutex)) {
+        TFW_LOGE_UTILS("TFW_Mutex_Unlock mutex is null");
+        ret = TFW_ERROR_INVALID_PARAM;
+    } else {
+        ret = TFW_Mutex_Unlock_Inner(mutex);
+        if (ret != 0) {
+            TFW_LOGE_UTILS("TFW_Mutex_Unlock failed, ret=%d", ret);
+            ret = TFW_ERROR_LOCK_FAILED;
+        }
+    }
+
+    return ret;
+}
